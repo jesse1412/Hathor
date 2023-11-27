5c327c5f48f7333bdf520b9dbd032841305b51e2
jesse1412 <jesseserrao@hotmail.co.uk>
2023-11-27 22:36:27 +0000

    Add media files for basic tests

--- crates/hathor-songs/src/database/initialise_db.rs
+++ crates/hathor-songs/src/database/initialise_db.rs
@@ -6,3 +6,16 @@ pub(crate) fn init_db(conn: &Connection) -> Result<(), Box<dyn std::error::Error
     conn.execute(include_str!("initialise_user_media_folders_table.sql"), ())?;
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use rusqlite::Connection;
+
+    use super::init_db;
+
+    #[test]
+    fn test_tables_created() {
+        let conn = Connection::open_in_memory().unwrap();
+        init_db(&conn).expect("Database setup error");
+    }
+}
