2d90b904fb223d314afdcf032b32154be210813d
jesse1412 <jesseserrao@hotmail.co.uk>
2023-11-27 11:54:54 +0000

    Add user media folder and playlist tables

--- crates/hathor-songs/src/database/initialise_db.rs
+++ crates/hathor-songs/src/database/initialise_db.rs
@@ -1,7 +1,8 @@
 use rusqlite::Connection;
 
 pub fn init_db(conn: &Connection) -> Result<(), Box<dyn std::error::Error>> {
-    let sql = include_str!("initialise_db.sql");
-    conn.execute(sql, ())?;
+    conn.execute(include_str!("initialise_playlists_table.sql"), ())?;
+    conn.execute(include_str!("initialise_songs_table.sql"), ())?;
+    conn.execute(include_str!("initialise_user_media_folders_table.sql"), ())?;
     Ok(())
 }
